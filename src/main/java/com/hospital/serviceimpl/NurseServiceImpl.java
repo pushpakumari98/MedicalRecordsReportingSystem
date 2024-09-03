5c5f7ae6d7f6a30eb77813564328695eecb69943
Pushpa Kumari <153999245+pushpakumari98@users.noreply.github.com>
2024-09-03 06:05:43 +0530

    Add files via upload

---
+++ src/main/java/com/hospital/serviceimpl/NurseServiceImpl.java
@@ -0,0 +1,48 @@
+package com.hospital.serviceimpl;
+
+
+import com.hospital.entity.Nurse;
+import com.hospital.repository.NurseRepository;
+
+import com.hospital.service.NurseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NurseServiceImpl implements NurseService {
+
+    @Autowired
+    private NurseRepository nurseRepository;
+    @Override
+    public Nurse saveNurse(Nurse nurse) {
+        return nurseRepository.save(nurse);
+    }
+
+    @Override
+    public List<Nurse> getAllNurse() {
+        return nurseRepository.findAll();
+    }
+
+    @Override
+    public Nurse getNurseById(Long nurseId) {
+        return nurseRepository.findById(nurseId).get();
+    }
+
+    @Override
+    public void deleteNurseById(Long id) {
+        nurseRepository.deleteById(id);
+    }
+
+    @Override
+    public Nurse findById(Long id) {
+        return nurseRepository.findById(id).get();
+    }
+
+    @Override
+    public Optional<Nurse> findByPhone(Long phone) {
+        return nurseRepository.findByPhone(phone);
+    }
+}
