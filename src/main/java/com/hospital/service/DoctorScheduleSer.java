481f670e8f7670a3a0781b06b7eec869aa85e89e
Pushpa Kumari <153999245+pushpakumari98@users.noreply.github.com>
2024-09-03 06:04:03 +0530

    Add files via upload

---
+++ src/main/java/com/hospital/service/DoctorScheduleSer.java
@@ -0,0 +1,10 @@
+package com.hospital.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DoctorScheduleSer {
+
+    //void createDoctorSchedule(DoctorSchedule doctorSchedule);
+
+}
