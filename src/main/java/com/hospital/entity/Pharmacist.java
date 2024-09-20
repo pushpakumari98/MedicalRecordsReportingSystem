6781e7599978b1104a17effe6794c6666b017679
pushpakumari98 <pushpa20052002kumari@gmail.com>
2024-09-20 16:44:11 +0530

    Test

--- src/main/java/com/hospital/entity/Pharmacist.java
+++ src/main/java/com/hospital/entity/Pharmacist.java
@@ -1,20 +1,69 @@
 package com.hospital.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+
 public class Pharmacist {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-
+        private Long Id;
         private String name;
         private int age;
         private String specialization;
         private Date dob;
+        private String phone;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "patient_id", referencedColumnName = "id")
